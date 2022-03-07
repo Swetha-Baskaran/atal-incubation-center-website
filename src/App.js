b149ded61237ce74335177501ab447a470bec4ec
swetha <swethasambothi@gmail.com>
2022-03-07 07:27:51 +0530

    carousel tag

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import CarouselComp from './components/carousel';
+
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 
@@ -8,21 +8,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-        <Carousel autoPlay showArrows={false} showIndicators={false} showThumbs={false} showStatus={false} stopOnHover={false} label={false} infiniteLoop={true} renderIndicator={false}>
-                <div>
-                    <img className="img" src="https://www.aicpecf.org/uploads/9/8/0/9/9809129/whatsapp-image-2021-03-05-at-13-17-27_orig.jpeg" />
-                    {/* <p className="legend">Legend 1</p> */}
-                </div>
-                <div>
-                    <img className="img" src="https://www.aicpecf.org/uploads/9/8/0/9/9809129/img-20180118-wa0035_orig.jpg" />
-                    {/* <p className="legend">Legend 2</p> */}
-                </div>
-                <div>
-                    <img className="img" src="https://www.aicpecf.org/uploads/9/8/0/9/9809129/2p6a4286-1_orig.jpg" />
-                    {/* <p className="legend">Legend 3</p> */}
-                </div>
-            </Carousel>
-            <Footer />
+      <CarouselComp />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/carousel/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
+import "./style.css"
+
+export default function CarouselComp(){
+    return (
+        <Carousel autoPlay  showArrows={false} showIndicators={false} showThumbs={false} showStatus={false} stopOnHover={false} label={false} infiniteLoop={true} renderIndicator={false}>
+                <div className="img-hold">
+                    <div className="img-text">
+                       <div className="img-text-part">
+                           <h2>Startup Inaugration</h2>    
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda debitis quae, explicabo voluptates magni consequuntur nesciunt similique placeat impedit eum unde doloribus natus officia harum cupiditate perferendis laudantium pariatur temporibus?</p>
+                       <button>View More</button>
+                       </div>
+                    </div>
+                    <img className="img" src="https://www.aicpecf.org/uploads/9/8/0/9/9809129/whatsapp-image-2021-03-05-at-13-17-27_orig.jpeg" />
+                    {/* <p className="legend">Legend 1</p> */}
+                </div>
+                <div className="img-hold">
+                <div className="img-text">
+                       <div className="img-text-part">
+                           <h2>Startup Inaugration</h2>    
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda debitis quae, explicabo voluptates magni consequuntur nesciunt similique placeat impedit eum unde doloribus natus officia harum cupiditate perferendis laudantium pariatur temporibus?</p>
+                       <button>View More</button>
+                       </div>
+                    </div>
+                    <img className="img" src="https://www.aicpecf.org/uploads/9/8/0/9/9809129/img-20180118-wa0035_orig.jpg" />
+                    {/* <p className="legend">Legend 2</p> */}
+                </div>
+                <div className="img-hold">
+                <div className="img-text">
+                       <div className="img-text-part">
+                           <h2>Startup Inaugration</h2>    
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda debitis quae, explicabo voluptates magni consequuntur nesciunt similique placeat impedit eum unde doloribus natus officia harum cupiditate perferendis laudantium pariatur temporibus?</p>
+                       <button>View More</button>
+                       </div>
+                    </div>
+                    <img className="img" src="https://www.aicpecf.org/uploads/9/8/0/9/9809129/2p6a4286-1_orig.jpg" />
+                    {/* <p className="legend">Legend 3</p> */}
+                </div>
+            </Carousel>
+    )
+} 
