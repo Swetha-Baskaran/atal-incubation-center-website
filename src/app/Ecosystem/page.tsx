2e79abe60525685b188a4caf36cd84cbd189ba30
SwethaBaskaran <swetha.b@cognived.com>
2023-01-27 11:12:50 +0530

    test

--- src/app/Ecosystem/page.tsx
+++ src/app/Ecosystem/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const page = () => {
+const EcoSystem = () => {
   return (
-    <div>page</div>
+    <div>EcoSystem</div>
   )
 }
 
-export default page
+export default EcoSystem
