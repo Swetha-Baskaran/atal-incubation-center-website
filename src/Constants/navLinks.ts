192f499cade770eae8cc51ab722372f345df6c2e
SwethaBaskaran <swetha.b@cognived.com>
2023-01-28 11:16:11 +0530

    navlinks

--- src/Constants/navLinks.ts
+++ src/Constants/navLinks.ts
@@ -1,3 +1,50 @@
-export const navLinks = {
-    
-}
+export const navLinks = [
+    {
+        name: "Home",
+        links: [],
+    },
+    {
+        name: "About",
+        links: [],
+    },
+    {
+        name: "Programs",
+        links: [],
+    },
+    {
+        name: "Funding",
+        links: [],
+    },
+    {
+        name: "Ecosystem",
+        links: [],
+    },
+    {
+        name: "CSR",
+        links: [],
+    },
+    {
+        name: "Stackholder",
+        links: [],
+    },
+    {
+        name: "Events",
+        links: [],
+    },
+    {
+        name: "Projects",
+        links: [],
+    },
+    {
+        name: "Tenders",
+        links: [],
+    },
+    {
+        name: "Get Involved",
+        links: [],
+    },
+    {
+        name: "Problem Statement",
+        links: [],
+    },
+]
